712ad11aa56d4e4aea52cf8ba6cfaf2634c4ad40
Clément Omen <clement.labbe33@gmail.com>
2023-12-01 10:58:02 +0100

    new , reset delete from pret.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 function init {
     if test -f data.json
     then
-        echo "[pret.sh] Data already exist. Are you sure you want to delete it? (y)"
+        echo "[Gegit] Data already exist. Are you sure you want to delete it? (y)"
         read -r resp
         test "$resp" = 'y' -o "$resp" = 'Y'  && printf '{ 
     "rep": []
@@ -14,4 +14,46 @@ function init {
     fi
 }
 
-function
+#add a new article identified by CODE with the description DESCRIPTION
+#checks if the item is already in the item list, if not
+#adds the item to the array
+function add {
+    name="$1"
+    link="$2"
+    test=$(jq --arg name "$name" '.rep | any(.name == $name)' data.json)
+    if [ "$test" == true ]; then
+        echo "[Gegit] Error : Depot ""$name"" already exists."
+        exit 1;
+    else
+        jq --arg name "$name" --arg link "$link" '.rep += [{ "name": $name, "link": $link }]' data.json > temp.json
+        mv temp.json data.json
+    fi
+    
+}
+
+
+
+#main content
+#call the function corresponding to the argument
+action="${1,,}"
+shift
+case $action in
+        init) 
+            init ;;
+        add) 
+            add "$@" ;;
+        lend) 
+            lend "$@";;
+        retrieve) 
+            retrieve "$@" ;;
+        list)
+            list "$@";;
+        *)
+
+            usage
+            exit 1
+    ;;
+esac
+
+
+exit 0
